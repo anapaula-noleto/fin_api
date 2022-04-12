394a4121d43aa2c33126f59f74bdc31e5c146ce1
Ana Paula Nazareno Noleto <apaulanazarenonoleto@gmail.com>
2022-04-12 15:09:26 -0300

    I've implemented the check banck statement feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express")
 const {v4: uuidv4} = require("uuid")
 
@@ -10,12 +11,25 @@ app.use(express.json())
 //criar um array que vai ser o banco de dados por hora
 const customers = []
 
-/* 
-cpf: string
-name: string
-id: uuid
-statement []
-*/
+//Middleware: uma função que fica entre o request e o response
+    // usado para fazer a validação de um token
+    // autenticação e validações
+function verifyIfExistsCPF(req, res, next){
+    // se o next não acontecer significa que não passou nas condições do middleware
+    const { cpf } = req.headers;
+    const customer = customers.find(client => client.cpf == cpf)
+    if(!customer){
+        res.status(400).json({error: "Customer not found"})
+    }
+
+    //passando a constante customer para que as rotas possam usá-la
+    req.customer = customer;
+    return next();
+}
+
+//app.use(verifyIfExistsCPF) -> serve para aplicar esse middleware em todas as rotas
+
+//criar conta e verificar se cliente já existe ou não
 app.post("/account", (req, res) => {
     const {cpf, name} = req.body
     // The some() method checks if any array elements pass a test (provided as a function).
@@ -24,16 +38,24 @@ app.post("/account", (req, res) => {
         return res.status(400).json({error: "Customer already exists!"})
     }
 
-    const customer = {
+    customers.push({
         cpf,
         name,
         id: uuidv4(),
-        statement: []
-    }
-
-    customers.push(customer)
-    return res.status(201).send(customer)
+        statement: [],
+    })
+    //console.log(customers)
+    return res.status(201).send()
 })
+
+//buscar o extrato bancário do cliente
+app.get("/statement",verifyIfExistsCPF, (req, res) => {
+    // recuperando o costumer do middleware
+    console.log(req)
+    const { customer } = req;
+    return res.json(customer.statement)
+})
+
 
 app.listen(80, () => {
     console.log('Servidor rodando na porta 80')
