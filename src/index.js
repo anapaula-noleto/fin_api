219fe026b2496f621958676efcd83d1857a27164
Ana Paula Nazareno Noleto <apaulanazarenonoleto@gmail.com>
2022-04-13 18:47:18 -0300

    more features

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { response } = require("express");
-const express = require("express")
+const express = require("express");
+const { get } = require("express/lib/response");
 const {v4: uuidv4} = require("uuid")
 
 const app = express();
@@ -27,9 +28,21 @@ function verifyIfExistsCPF(req, res, next){
     return next();
 }
 
+// função de obter o saldo
+function getBalance(statement){
+    const balance = statement.reduce( (acc, operation) => {
+        if(operation.type === 'deposit'){
+            return acc + operation.amount
+        }else {
+            return acc - operation.amount;
+        }
+    }, 0)
+    return balance
+}
+
 //app.use(verifyIfExistsCPF) -> serve para aplicar esse middleware em todas as rotas
 
-//criar conta e verificar se cliente já existe ou não
+//rota para criar conta e verificar se cliente já existe ou não
 app.post("/account", (req, res) => {
     const {cpf, name} = req.body
     // The some() method checks if any array elements pass a test (provided as a function).
@@ -48,14 +61,61 @@ app.post("/account", (req, res) => {
     return res.status(201).send()
 })
 
-//buscar o extrato bancário do cliente
+//rota para buscar o extrato bancário do cliente
 app.get("/statement",verifyIfExistsCPF, (req, res) => {
     // recuperando o costumer do middleware
-    console.log(req)
     const { customer } = req;
     return res.json(customer.statement)
 })
 
+//rota para realizar um depósito
+app.post("/deposit", verifyIfExistsCPF, (req, res) => {
+    const {description, amount} = req.body;
+    const {customer} = req
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "deposit",
+    }
+    customer.statement.push(statementOperation)
+    return res.status(201).send()
+})
+
+app.post("/withdraw", verifyIfExistsCPF, (req, res) => {
+    const {amount} = req.body
+    const {customer} = req
+
+    const balance = getBalance(customer.statement);
+
+    if(amount > balance){
+        return res.status(400).json({error: 'Insufficient funds!'})
+    }
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "withdraw",
+    }
+    customer.statement.push(statementOperation)
+    return res.status(201).send()
+
+})
+
+app.get("/statement/date", verifyIfExistsCPF, (req, res) => {
+    const { customer } = req
+    const { date } = req.query
+
+    //por estar apenas interessado na data e não na hora tudo está sendo normalizado para 00:00
+    const dateFormat = new Date(date + " 00:00")
+
+    const statement = customer.statement.filter( statement => 
+    statement.created_at.toDateString() === 
+    dateFormat.toDateString()
+    )
+
+    return res.json(statement)
+})
 
 app.listen(80, () => {
     console.log('Servidor rodando na porta 80')
